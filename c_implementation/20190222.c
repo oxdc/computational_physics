96a11229970130f8c9ae93d94c59a4e9779b31dc
oxdc <frnet2016@gmail.com>
2019-02-27 10:11:47 +0800

    Add project files.

---
+++ c_implementation/20190222.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <float.h>
+
+#define BYTE_LENGTH CHAR_BIT
+#define FLT_LENGTH (sizeof(float) * BYTE_LENGTH)
+#define DBL_LENGTH (sizeof(double) * BYTE_LENGTH)
+
+typedef unsigned char Bit;
+typedef unsigned char Byte;
+
+void toBitArray(void* buffer, size_t length, Bit* dest)
+{
+	Byte* p_buffer = (Byte*)buffer;
+	for (int c = (int)length - 1; c >= 0; c--)
+	{
+		for (int i = (int)BYTE_LENGTH - 1; i >= 0; i--)
+		{
+			dest[(length - c) * BYTE_LENGTH - i - 1] = ((p_buffer[c] >> i) & 0x1);
+		}
+	}
+}
+
+#define printBinary(value, type) \
+{ \
+	const size_t LENGHT = sizeof(type) * BYTE_LENGTH; \
+	Byte* bits = (Byte*)malloc(LENGHT); \
+	type v = value; \
+	toBitArray((void*)& v, sizeof(type), (void*)bits); \
+	for (size_t i = 0; i < LENGHT; i++) \
+	{ \
+		if (i % 4 == 0 && i != 0) printf(" "); \
+		printf("%u", bits[i]); \
+	} \
+	printf("\n"); \
+	free(bits); \
+}
+
+int main(void) // `void` shouldn't be omitted!
+{
+	printBinary(1.0 / 8.0, float);
+	printBinary(-1.0 / 8.0, float);
+	printBinary(0.375, float);
+
+	system("pause");
+
+	return 0;
+}
+
+// At the end of a file, leave an empty line PLEASE!
